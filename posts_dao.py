5851bc515d12e2fe1070ff102ad813df3fbc5d57
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-04-05 21:53:28 +0600

    chore: DAO быть. Первые функции чтения

---
+++ posts_dao.py
@@ -0,0 +1,40 @@
+import json
+
+from config import *
+
+
+class PostsDAO:
+
+    def __init__(self):
+        self.posts_list = None
+        self.comments_list = None
+
+    def load_posts(self):
+        with open(POSTS_JSON, 'r', encoding='utf-8') as file:
+            self.posts_list = json.load(file)
+
+    def load_comments(self):
+        with open(COMMENTS_JSON, 'r', encoding='utf-8') as file:
+            self.comments_list = json.load(file)
+
+    def get_posts(self):
+        self.load_posts()
+        return self.posts_list
+
+    def get_comments(self):
+        self.load_comments()
+        return self.comments_list
+
+    def load_all(self):
+        self.get_posts()
+        self.get_comments()
+
+    def get_posts_by_user(self, user_name):
+        pass
+
+
+
+dao = PostsDAO()
+posts_list = dao.load_all()
+print(dao.comments_list)
+print(dao.posts_list)

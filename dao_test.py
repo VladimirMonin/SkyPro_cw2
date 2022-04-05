23f9446cc4488fad4eaba3773db0e3d7f05a7c4e
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-04-05 23:55:16 +0600

    chore: Шаг 0 готов (вроде))

--- dao_test.py
+++ dao_test.py
@@ -6,13 +6,20 @@ dao = PostsDAO()
 
 class TestPostDAO:
 
-    def test_create_postsDAO_object2(self):
+    def test_create_postsDAO_object(self):
         assert isinstance(dao, PostsDAO) == True
 
     def test_get_posts(self):
         assert isinstance(dao.get_posts(), list) == True
 
+    def test_get_comments(self):
+        assert isinstance(dao.get_comments(), list) == True
 
+    def test_get_posts_by_user(self, data=1):
+        assert isinstance(dao.get_posts_by_user(data), list) == True
 
-# dao = PostsDAO()
-# print(type(dao))
+    def test_get_post_by_keyword(self, data='ЕДА'):
+        assert isinstance(dao.get_post_by_keyword(data), list) == True
+
+    def test_get_post_by_pk(self, data=1):
+        assert isinstance(dao.get_post_by_pk(data), list) == True

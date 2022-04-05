aaef6ea96013ccefcbf7826ca4391f8bd5918420
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-04-05 12:57:16 +0600

    chore: Обозначил функции utils.py для первого шага

---
+++ utils.py
@@ -0,0 +1,23 @@
+def get_posts_all():
+    """Возвращает посты"""
+    pass
+
+
+def get_post_by_user(username):
+    """Возвращает посты конкретного юзера"""
+    pass
+
+
+def get_comments_by_post_id(post_id):
+    """Возвращает комментарии определенного поста"""
+    pass
+
+
+def search_for_posts(query):
+    """Возвращает список постов по ключевому слову"""
+    pass
+
+
+def get_post_by_pk(pk):
+    """Возвращает один пост по его идентификатору"""
+    pass

6bbd697c44300eeb80d3e006bfdbbeb4855d8be9
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-04-05 22:29:04 +0600

    chore: Базовые DAO методы готовы и прокомментированы

--- posts_dao.py
+++ posts_dao.py
@@ -10,26 +10,32 @@ class PostsDAO:
         self.comments_list = None
 
     def load_posts(self):
+        """Метод загружает файл с постами и сохраняет их в поле объекта"""
         with open(POSTS_JSON, 'r', encoding='utf-8') as file:
             self.posts_list = json.load(file)
 
     def load_comments(self):
+        """Метод загружает файл с комментами и сохраняет их в поле объекта"""
         with open(COMMENTS_JSON, 'r', encoding='utf-8') as file:
             self.comments_list = json.load(file)
 
     def get_posts(self):
+        """Метод загружает данные по постам в поле объекта и возвращает список словарей с постами """
         self.load_posts()
         return self.posts_list
 
     def get_comments(self):
+        """ Метод загружает данные по комментам в поле объекта и возвращает список словарей с комментами"""
         self.load_comments()
         return self.comments_list
 
     def get_all(self):
+        """Метод обновляет поля объекта данными с Json посты и комменты"""
         self.get_posts()
         self.get_comments()
 
     def get_posts_by_user(self, user_name):
+        """ Метод обновляет поле с постами, а потом возвращает список словарей постов конкретного пользователя """
         user_posts_list = []
         self.get_posts()
         for post in self.posts_list:
@@ -38,6 +44,7 @@ class PostsDAO:
         return user_posts_list
 
     def get_comments_by_post_id(self, post_id):
+        """ Метод обновляет поле с комментами, а потом возвращает комменты принадлежащие конкретному посту"""
         comments_list = []
         self.get_comments()
         for comment in self.comments_list:
@@ -46,6 +53,7 @@ class PostsDAO:
         return comments_list
 
     def get_post_by_keyword(self, keyword):
+        """Метод обновляет поле с постами, а потом возвращает пост с искомыми словами в тексте поста"""
         post_list = []
         self.get_posts()
         for post in self.posts_list:
@@ -53,6 +61,14 @@ class PostsDAO:
                 post_list.append(post)
         return post_list
 
+    def get_post_by_pk(self, pk):
+        """Метод обновляет поле с постами, а потом возвращает пост по его ID в виде списка"""
+        post_list = []
+        self.get_posts()
+        for post in self.posts_list:
+            if post['pk'] == pk:
+                post_list.append(post)
+        return post_list
 
 dao = PostsDAO()
-print(dao.get_post_by_keyword('УТРОМ'))
+print(dao.get_post_by_pk(8))

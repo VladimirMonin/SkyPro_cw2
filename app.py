e4e6b64bb96da511c06f59bbdf700d0704ddcb53
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-04-09 11:16:29 +0600

    chore: Доделал поиск т.е. шаг 3. Надо поправить вывод аватарок?

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, render_template
 from posts_dao import PostsDAO
+# Импортируем логирование
+import logging
+# Включаем логирование
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
 dao = PostsDAO()
 app = Flask(__name__)
@@ -17,11 +21,29 @@ def get_post_by_id(post_id):
     post_comments_list = single_post_dict['comments']  # Список словарей с комментами (пробовал уже и так вытащить)
     return render_template('post.html', post=single_post_dict, comments=post_comments_list)
 
+@app.route('/search/')
+def get_search():
+    search_str = request.args.get('s')
+    logging.info(f'Поисковая фраза: {search_str}')
+
+    post_list = dao.get_post_by_keyword(search_str)[:10]
+    logging.info(f'Поисковый список: {post_list}')
+    search_count = len(post_list)
+
+    return render_template('search.html', post_list=post_list, scount=search_count)
 
 app.run(debug=True)
 
 """
-Главная
+Bookmarks.html - закладки
+Index.html - главная
+Post.html - страница просмотра 1 поста
+Search.html - страница результатов поиска
+Tag.html - страница поиска по тэгу
+"""
+
+"""
+Index.html - главная
 Надо дать на главную
 1. Аватарка автора 31 строка
 2. Ник автора 33 строка
@@ -30,7 +52,7 @@ app.run(debug=True)
 4. Количество просмотров 43 строка
 5. Колличество комментов 47 строка
 
-Пост
+Post.html - страница просмотра 1 поста
 1. Ава 32 строка
 НИК автора 34 строка
 2. Картинка поста 41 строка
@@ -38,13 +60,19 @@ app.run(debug=True)
 4. Количество просмотров 55
 5. Количество комментариев 52
 6. Текст поста 45 (ПОЧЕМУ ДВАЖДЫ ИДЕТ???)
-"""
+
+1. Ава 38 строка
+2. Ник 40 строка
+3. Картинка поста 44
+4. Тэги 46 строка
+5. Текст описания короткий 46 строка
+6. Комментариев 48 строка
+7. Количество просмотров 51 строка
+
+
+МЕТОДЫ
+Метод получает ОДИН пост, ищет хештеги, делает стрип и откладывает их в список
+Метод получает используя метод выше пробегается по тексту поста и меняет слова на урлы
 
 """
-Bookmarks.html - закладки
-Index.html - главная
-Post.html - страница добавления постам
-Search.html - страница результатов поиска
-Tag.html - страница поиска по тэгу
 
-"""

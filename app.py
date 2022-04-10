68ee74e874d231494ea6e40d96c6f9110de20058
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-04-10 22:39:23 +0600

    fix: pep8 и комменты для app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from posts_dao import PostsDAO
 # Импортируем логирование
 import logging
+
 # Включаем логирование
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
@@ -9,18 +10,22 @@ dao = PostsDAO()
 app = Flask(__name__)
 
 
+#  ВЬЮШКА ГЛАВНОЙ СТРАНИЦЫ
 @app.route('/')
 def get_main_page():
     posts_list = dao.get_main_page()
     return render_template('index.html', posts_list=posts_list)
 
 
+#  ВЬЮШКА СТРАНИЦЫ 1 ПОСТА (по ID)
 @app.route('/posts/<int:post_id>')
 def get_post_by_id(post_id):
     single_post_dict = dao.get_post_by_pk(post_id)  # Словарь с данными по ОДНОМУ посту
     return render_template('post.html', post=single_post_dict)
 
-@app.route('/search/')
+
+#  ВЬЮШКА СТРАНИЦЫ ПОИСКА
+@app.route('/search/')  # http://127.0.0.1:5000/search/?s=%D0%B5%D0%B4%D0%B0
 def get_search():
     search_str = request.args.get('s')
     logging.info(f'Поисковая фраза: {search_str}')
@@ -31,47 +36,5 @@ def get_search():
 
     return render_template('search.html', post_list=post_list, scount=search_count)
 
+
 app.run(debug=True)
-
-"""
-Bookmarks.html - закладки
-Index.html - главная
-Post.html - страница просмотра 1 поста
-Search.html - страница результатов поиска
-Tag.html - страница поиска по тэгу
-"""
-
-"""
-Index.html - главная
-Надо дать на главную
-1. Аватарка автора 31 строка
-2. Ник автора 33 строка
-КАРТИНКА ПОСТА  38 строка?
-3. Укороченный(!) текст поста (до скольки знаков?) 39 строка
-4. Количество просмотров 43 строка
-5. Колличество комментов 47 строка
-
-Post.html - страница просмотра 1 поста
-1. Ава 32 строка
-НИК автора 34 строка
-2. Картинка поста 41 строка
-3. Комменты около 60 строки
-4. Количество просмотров 55
-5. Количество комментариев 52
-6. Текст поста 45 (ПОЧЕМУ ДВАЖДЫ ИДЕТ???)
-
-1. Ава 38 строка
-2. Ник 40 строка
-3. Картинка поста 44
-4. Тэги 46 строка
-5. Текст описания короткий 46 строка
-6. Комментариев 48 строка
-7. Количество просмотров 51 строка
-
-
-МЕТОДЫ
-Метод получает ОДИН пост, ищет хештеги, делает стрип и откладывает их в список
-Метод получает используя метод выше пробегается по тексту поста и меняет слова на урлы
-
-"""
-
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-POSTS_JSON = 'data/posts.json'
-COMMENTS_JSON = 'data/comments.json'
-BOOKMARKS_JSON = 'data/bookmarks.json'
-STRIP_SYM_IN_TAG = '!?,.@'
+POSTS_JSON = 'data/posts.json'  # Файл с постами
+COMMENTS_JSON = 'data/comments.json'  # Файл с комментами
+BOOKMARKS_JSON = 'data/bookmarks.json'  # Файл с закладками
+STRIP_SYM_IN_TAG = '!?,.@'  # Символы для "обрезания" при поиски хештегов
